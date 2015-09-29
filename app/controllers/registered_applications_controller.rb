2f3ddf0f5af6206a885b7007cc235afbb03328c8
Philip Speake <philip_speake@yahoo.com>
2015-09-29 12:09:17 -0700

    completed checkpoint-4

---
+++ app/controllers/registered_applications_controller.rb
@@ -0,0 +1,55 @@
+class RegisteredApplicationsController < ApplicationController
+  def index
+    @registered_applications = RegisteredApplication.all
+  end
+
+  def show
+    @registered_application = RegisteredApplication.find(params[:id])
+  end
+
+  def new
+     @registered_application = RegisteredApplication.new
+  end
+
+
+  def create
+      @registered_application = RegisteredApplication.new(params.require(:registered_application).permit(:name, :url))
+     if @registered_application.save
+       flash[:notice] = "Registered application was saved."
+       redirect_to registered_applications_path
+     else
+       flash[:error] = "There was an error saving the registered application. Please try again."
+       render :new
+     end
+   end
+
+  def edit
+      @registered_application = RegisteredApplication.find(params[:id])
+
+     
+  end
+
+  def update
+      @registered_application = RegisteredApplication.find(params[:id])
+       if @registered_application.update_attributes(params.require(:registered_application).permit(:name, :url))
+       flash[:notice] = "Registered application was saved."
+       redirect_to registered_applications_path
+     else
+       flash[:error] = "There was an error saving the registered application. Please try again."
+       render :edit
+     end
+  end
+
+  def destroy
+    @registered_application = RegisteredApplication.find(params[:id])
+
+    if @registered_application.destroy
+       flash[:notice] = "Registered application was saved."
+       redirect_to @registered_application
+     else
+       flash[:error] = "There was an error saving the registered application. Please try again."
+       render :index
+     end
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  
+
+  resources :registered_applications
+
   devise_for :users
+  
   get 'welcome/index'
 
   get 'welcome/about'
